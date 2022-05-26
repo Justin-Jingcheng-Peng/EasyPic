463106e175136c407b254105e1c6d9a11ee256d3
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-05-25 22:53:51 -0300

    Finished search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { MainProvider } from "./context/MainContext";
 
 function App() {
-  const [keyword, setKeyword] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-    </div>
+    <MainProvider>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+      </div>
+    </MainProvider>
   );
 }
 
---
+++ frontend/src/context/MainContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+const MainContext = createContext();
+
+export const MainProvider = ({ children }) => {
+  //This global state is to track if the app is in "edit" mode;
+
+  const [search, setSearch] = useState("Test");
+
+  return (
+    <MainContext.Provider
+      value={{
+        search,
+        setSearch,
+      }}
+    >
+      {children}
+    </MainContext.Provider>
+  );
+};
+
+export default MainContext;
